0ee5d31ae6b266b765557b721091bbb99f0b15a7
lotemh <lotemh@wix.com>
2016-01-12 13:57:53 +0200

    adding public directory

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-//app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 //app.set('views', __dirname + '/views');
 //app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('index.html');
+  response.render('index');
 });
 
 app.listen(app.get('port'), function() {
